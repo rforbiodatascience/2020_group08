a5919485e378bae8f89722f4e6601e86ad453c51
mathiasfessler <matfe@env.dtu.dk>
2020-04-08 20:08:29 +0000

    Cleaned data

--- R/01_load.R
+++ R/01_load.R
@@ -1,5 +1,20 @@
-install.packages("xlsx")
+# Clear workspace
+# ------------------------------------------------------------------------------
+rm(list = ls())
 
+# Install packages and load libraries
+# ------------------------------------------------------------------------------
+install.packages("xlsx")
 library(xlsx)
 
-prostate <- read.xlsx("_raw/prostate.xlsx", sheetIndex = 1)
+install.packages("tidyverse")
+library(tidyverse)
+
+# Load data
+# ------------------------------------------------------------------------------
+prostate_data <- read.xlsx("Data/_raw/prostate.xlsx", sheetIndex = 1)
+
+# Write data
+# ------------------------------------------------------------------------------
+write_tsv(x = prostate_data,
+          path = "Data/01_prostate_data.tsv")
---
+++ R/02_clean.R
@@ -0,0 +1,45 @@
+# Clear workspace
+# ------------------------------------------------------------------------------
+rm(list = ls())
+
+# Load libraries
+# ------------------------------------------------------------------------------
+library(tidyverse)
+
+install.packages("tibble")
+library(tibble)
+
+library(stringr)
+
+# Load data
+# ------------------------------------------------------------------------------
+prostate_data <- read_tsv(file = "Data/01_prostate_data.tsv") %>% 
+  as_tibble()
+
+# Wrangle data
+# ------------------------------------------------------------------------------
+
+#Add columns
+prostate_data_clean <- 
+  prostate_data %>%
+  mutate(estrogen_mg = case_when(rx == "0.2 mg estrogen" ~ 0.2,
+                                 rx == "1.0 mg estrogen" ~ 1,
+                                 rx == "5.0 mg estrogen" ~ 5,
+                                 rx == "placebo" ~ 0),
+         status_ = case_when(status == "alive" ~ "alive",
+                             str_detect(status, "dead") ~ "dead"),
+         cause_of_death = case_when(status == "dead - other ca" ~ "other cancer",
+                                    status == "dead - cerebrovascular" ~ "cerebrovascular disease/accident",
+                                    status == "dead - prostatic ca" ~ "prostate cancer",
+                                    status == "dead - heart or vascular" ~ "heart or vascular disease",
+                                    status == "alive" ~ "N/A",
+                                    status == "dead - pulmonary embolus" ~ "pulmonary embolus",
+                                    status == "dead - other specific non-ca" ~ "other specified non-cancer",
+                                    status == "dead - unknown cause" ~ "unknown",
+                                    status == "dead - unspecified non-ca" ~ "unspecified non-cancer",
+                                    status == "dead - respiratory disease" ~ "respiratory disease")) 
+
+#Remove columns
+prostate_data_clean$rx <- NULL
+prostate_data_clean$status <- NULL
+
