435a883e60bd98b90f288f54664ae6e72bf9ee98
sehwadollee <sehwadollee@gmail.com>
2020-04-08 12:30:11 +0000

    date change according to the website

--- R/02_clean.R
+++ R/02_clean.R
@@ -1,3 +1,10 @@
 #install packages
 
 install.packages("tidyverse")
+
+#variables to be changed- rx--> placebo, 
+#recent MIS, 
+install.packages("chron")
+library(chron)
+ as.Date(prostate$sdate)
+?as.Date
