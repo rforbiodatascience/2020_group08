09d1ce9f5ed07064097d67ce3b88f4d5f035d4f7
mathiasfessler <matfe@env.dtu.dk>
2020-04-09 09:34:55 +0000

    renamed columns and removed wrong value in PA_phosphatase column

--- R/02_clean.R
+++ R/02_clean.R
@@ -37,9 +37,18 @@ prostate_data_clean <-
                                     status == "dead - other specific non-ca" ~ "other specified non-cancer",
                                     status == "dead - unknown cause" ~ "unknown",
                                     status == "dead - unspecified non-ca" ~ "unspecified non-cancer",
-                                    status == "dead - respiratory disease" ~ "respiratory disease")) 
+                                    status == "dead - respiratory disease" ~ "respiratory disease")) %>% 
+  na_if("N/A")
 
 #Remove columns
 prostate_data_clean$rx <- NULL
 prostate_data_clean$status <- NULL
 
+#Rename columns
+prostate_data_clean <- prostate_data_clean %>% 
+  rename(months_of_follow_up = dtime, weight_index = wt, activity = pf, history_of_CD = hx,
+         serum_hemoglobin = hg,tumor_size = sz, stage_grade_index = sg, PA_phosphatase = ap, 
+         bone_metastases = bm, systolic_bp = sbp, diastolic_bp = dbp)
+
+#Convert value to NA, which was wrongly assigned a value according to the authors of the study
+prostate_data_clean <- na_if(prostate_data_clean, 999.87500000)
