83de85b67f570726f65f8d7c6dd4d8cf916b896d
mathiasfessler <matfe@env.dtu.dk>
2020-04-10 18:32:56 +0000

    Updated 04_analysis_i.R

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -11,8 +11,10 @@ library(ggplot2)
 # ------------------------------------------------------------------------------
 prostate_data_clean <- read_tsv(file = "Data/02_prostate_data_clean.tsv")
 
-# Wrangle data
+# Wrangle and analyse data
 # ------------------------------------------------------------------------------
+
+#Plots for at quick overview
 
 # Looking at the PA_phosphatase variable
 # Wrangle to long format omitting PA_phospahtase, since this variable is investigated
@@ -41,3 +43,60 @@ pdc_long_PAphosphatase %>%
   ggplot(aes(y = PA_phosphatase, x = value, color = cause_of_death)) +
   geom_point() +
   facet_wrap(~ variable, ncol = 3)
+
+
+
+# Looking at the bone_metastases variable (some interesting trends here)
+pdc_long_BoneMetastases <- prostate_data_clean %>% 
+  pivot_longer(-c(patno, bone_metastases, activity, status_, cause_of_death, ekg, sdate),
+               names_to = "variable",
+               values_to = "value")
+
+pdc_long_BoneMetastases %>% 
+  ggplot(aes(y = bone_metastases, x = value, color = activity)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+pdc_long_BoneMetastases %>% 
+  ggplot(aes(y = bone_metastases, x = value, color = ekg)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+pdc_long_BoneMetastases %>% 
+  ggplot(aes(y = bone_metastases, x = value, color = status_)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+pdc_long_BoneMetastases %>% 
+  ggplot(aes(y = bone_metastases, x = value, color = cause_of_death)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+
+
+# Looking at the estrogen variable
+pdc_long_estrogen <- prostate_data_clean %>% 
+  pivot_longer(-c(patno, estrogen_mg, activity, status_, cause_of_death, ekg, sdate),
+               names_to = "variable",
+               values_to = "value")
+
+pdc_long_estrogen %>% 
+  ggplot(aes(x = estrogen_mg, y = value, color = activity)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+pdc_long_estrogen %>% 
+  ggplot(aes(x = estrogen_mg, y = value, color = ekg)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+pdc_long_estrogen %>% 
+  ggplot(aes(x = estrogen_mg, y = value, color = status_)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
+pdc_long_estrogen %>% 
+  ggplot(aes(x = estrogen_mg, y = value, color = cause_of_death)) +
+  geom_point() +
+  facet_wrap(~ variable, ncol = 3)
+
