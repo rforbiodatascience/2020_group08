8dac34336cfca3a3ebaf25add362f493e16c80d9
sehwadollee <sehwadollee@gmail.com>
2020-04-12 08:13:56 +0000

    model

--- 3_augment_R.R
+++ 3_augment_R.R
@@ -69,9 +69,11 @@ by_weightindex<-by_weightindex %>%mutate(mdls=map(data, model_weightindex)) %>%
 
 by_weightindex %>% 
   arrange(desc(r.squared)) %>% 
-  filter(r.squared >= 0.8) %>% 
-  unnest()
+  filter(r.squared >= 0.8)  
   
+#know that those 10 datasets are good fit
+
+
   
 
 
