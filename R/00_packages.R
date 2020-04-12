21a0beaf0d1cc894ccd8ced30818ac58eb464747
sehwadollee <sehwadollee@gmail.com>
2020-04-12 16:20:48 +0000

    dddddd

--- R/00_packages.R
+++ R/00_packages.R
@@ -1,8 +1,6 @@
 #install packages required 
 
 install.packages("tidyverse")
-install.packages("dplyr")
-install.packages("tibble")
-install.packages("readr")
 install.packages("xlsx")
 install.packages("ggplot2")
+install.packages("stringr")
