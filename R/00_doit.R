f88d1f624cfe0a30f19319ef354c1554333b7642
mathiasfessler <matfe@env.dtu.dk>
2020-05-12 09:48:16 +0000

    made 00_doit.R and checked it worked

---
+++ R/00_doit.R
@@ -0,0 +1,10 @@
+# Run all scripts
+# ------------------------------------------------------------------------------
+source(file = "R/01_load.R")
+source(file = "R/02_clean.R")
+source(file = "R/03_augment.R")
+source(file = "R/04_glm_death_prediction.R")
+source(file = "R/05_lm(sbp_vs_dbp).R")
+source(file = "R/06_metastases_plots.R")
+source(file = "R/07_PCA.R")
+source(file = "R/08_keras_ANN.R")
